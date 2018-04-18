09fe3b0a3f96dbdf2290d4dcd31993dc67f8efe3
Alex Kim <askim1019@gmail.com>
2018-04-18 00:46:44 -0500

    Get correct and wrong answer functions to work

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -43,9 +43,135 @@ questions = [
 ];
 
 console.log(questions);
+var interval;
+var runClock = false;
+var count = 15;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unAnswered = 0;
+var obj = 0;
+var wrongAnswerMsg;
+var correctAnswerMsg;
+
+
+// Separate a function for showing the time left ... make sure to clear interval after the question is answered or timer is 0.
+function timer() {
+    interval = setInterval(function(){
+        count--;
+        $("#timer").html("<h3>You have " + count +  " seconds</h3>");
+        if (count <= 0 || runClock) {
+            clearInterval(interval);
+            outOfTime();
+            count = 15;
+            unAnswered++;
+            obj++;
+        }
+    }, 1000);
+}
+
+// DOM selector to show question
+function showQuestion() {
+    $("#questionDiv").html("<h3>" + questions[obj].question + "</h3>");
+}
+
+// show the possibilities from our objects array
+function showChoices() {
+    for (var j = 0; j < questions[obj]["choices"].length; j++) {
+        $("#choicesDiv").append("<p>" + questions[obj].choices[j] + "</p>");
+
+        if (questions[obj].choices[j] === questions[obj].answer) {
+            $(`p:eq(${j})`).addClass("correct");
+        }
+    }
+}
+// clear everything before the next question
+function clearDivs() {
+    $("#timer").empty();
+    $("#questionDiv").empty();
+    $("#choicesDiv").empty();
+    $("#message").empty();
+    $("#picture").empty();
+    $("#amountCorrect").empty();
+    $("#amountWrong").empty();
+    $("#amountUnanswered").empty();
+}
+
+// when timer runs to 0...
+function outOfTime() {
+    clearDivs();
+    unAnswered++;
+}
+// show a function when you pick correctly
+function correctPick() {
+    clearDivs();
+    $("#message").html("<h3>" + correctAnswerMsg + "</h3>");
+    $("#message > h3").css("color", "green");
+    $("#answer").show();
+
+    setTimeout(function(){
+        clearDivs();
+        correctAnswers++;
+        obj++;
+        nextQuestion();
+    },3000); 
+}
+
+// show a function when the pick is wrong
+function wrongPick() {
+    clearDivs();
+    $("#answer").show();
+    $("#message").append(`<h3> ${wrongAnswerMsg} </h3>`);
+    
+    setTimeout(function(){
+        clearDivs();
+        wrongAnswers++;
+        obj++;
+        nextQuestion();
+    },3000); 
+
+}
+
+// create a function that grabs the next question
+function nextQuestion() {
+    console.log(unAnswered)
+    console.log(correctAnswers);
+    console.log(wrongAnswers);
+    count = 15;
+    wrongAnswerMsg = `Sorry! That is incorrect! The correct answer is ` + questions[obj].answer;
+    correctAnswerMsg = `Way to go! That is the correct answer!`;
+    clearInterval(interval);
+    console.log(count);
+    $("#content").show();
+    
+    if (obj < questions.length) {
+        showQuestion();
+        showChoices();
+        timer();
+
+        $("p").click(function(){
+            clearInterval(interval);
+            if ($(this).hasClass("correct")) {
+                correctPick();
+            } else {
+                wrongPick();
+            }
+        });
+    }
+
+}
+
+// initializes beginning of game
 
 $(document).ready(function(){
-
-
+    clearInterval(interval);
+    $("#content").hide();
+    $("#answer").hide();
+    $("#results").hide();
+    
+    $("#startButton").click(function(){
+        $(this).hide();
+    
+        nextQuestion();
+    });
     
 });
